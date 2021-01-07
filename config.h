7b93a5becdad23d97a3d6d2ef6b0b604fb2437e9
mao_64 <michaelaorndorff@gmail.com>
2021-01-06 21:26:30 -0500

    update

--- config.h
+++ config.h
@@ -5,11 +5,11 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Inconsolata:pixelsize=18:antialias=true:autohint=true";
+static char *font = "Source Code Pro:pixelsize=18:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true",
-	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
+	"Font Awesome:pixelsize=11:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;
@@ -197,16 +197,16 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+	{ TERMMOD,              XK_I,       	zoom,        	{.f = +1} },
+	{ TERMMOD,              XK_O,       	zoom,	        {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ShiftMask,            XK_K,    		 kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_J,			  	 kscrolldown,    {.i = -1} },
+	{ TERMMOD,	        XK_K,    	kscrollup,      {.i = 1} },
+	{ TERMMOD,          	XK_J,		kscrolldown,    {.i = 1} },
 };
 
 /*
